51bff5e43076240cb9ce47d717a9a58fd1477550
meherhendi <meherhendi0@gmail.com>
2020-03-01 16:56:09 +0100

    11 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,35 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CertificationComponent } from './certification/certification.component';
+import { CertifDetailsComponent } from './certification/certif-details/certif-details.component';
+import { CertifFormsComponent } from './certification/certif-forms/certif-forms.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ImageUrlComponent } from './certification/certif-forms/image-url/image-url.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    CertificationComponent,
+    
+    CertifDetailsComponent,
+    
+    CertifFormsComponent,
+    
+    ImageUrlComponent ,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule ,
+    FormsModule ,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
